d293f55e158defb0baf061fadc3ce25ba955698a
nguyentuanthanh89 <ntuanthanh745@gmail.com>
2022-06-30 12:40:30 +0700

    Add project files.

---
+++ DOAN_WEB_1/Controllers/SanphamController.cs
@@ -0,0 +1,36 @@
+﻿using DOAN_WEB_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using PagedList.Mvc;
+
+namespace DOAN_WEB_1.Controllers
+{
+    public class SanphamController : Controller
+    {
+        DataQLWebIPDataContext data = new DataQLWebIPDataContext();
+        private List<SANPHAM> Layiphonemoi(int count)
+        {
+            return data.SANPHAMs.OrderByDescending(a => a.NgayCapNhap).Take(count).ToList();
+        }
+        public ActionResult Index(int ? page)
+        {
+            int pageSize = 3;
+            int pageNum = (page ?? 1);
+
+            var iphonemoi = Layiphonemoi(7);
+            return View(iphonemoi.ToPagedList(pageSize,pageNum));
+        }
+        // GET: Sanpham
+        public ActionResult Detail(int id)
+        {
+            var iphone = from x in data.SANPHAMs
+                       where x.MaSP == id
+                       select x;
+            return View(iphone.Single());
+        }
+    }
+}
---
+++ DOAN_WEB_1/Models/NguoidungController.cs
@@ -0,0 +1,111 @@
+﻿using DOAN_WEB_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DOAN_WEB_1.Models
+{
+    public class NguoidungController : Controller
+    {
+        DataQLWebIPDataContext db = new DataQLWebIPDataContext();
+        // GET: Nguoidung
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult DangKy()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DangKy(FormCollection collection, KHACHHANG kh)
+        {
+            var hoten = collection["HotenKH"];
+            var tendn = collection["TenDN"];
+            var matkhau = collection["Matkhau"];
+            var matkhaunhaplai = collection["Matkhaunhaplai"];
+            var email = collection["Email"];
+            var diachi = collection["Diachi"];
+            var dienthoai = collection["Dienthoai"];
+
+            if (String.IsNullOrEmpty(hoten))
+            {
+                ViewData["Loi1"] = "Họ tên khách hàng không được để trống";
+            }
+            else if (String.IsNullOrEmpty(tendn))
+            {
+                ViewData["Loi2"] = "Phải nhập tên đăng nhập";
+            }
+            else if (String.IsNullOrEmpty(matkhau))
+            {
+                ViewData["Loi3"] = "Phải nhập mật khẩu"; ;
+            }
+            else if (String.IsNullOrEmpty(matkhaunhaplai))
+            {
+                ViewData["Loi4"] = "Phải nhập lại mật khẩu";
+            }
+
+            if (String.IsNullOrEmpty(email))
+            {
+                ViewData["Loi5"] = "Email không được bò trống";
+            }
+
+            if (String.IsNullOrEmpty(diachi))
+            {
+                ViewData["Loi6"] = "Phải nhập địa chỉ";
+            }
+
+            if (String.IsNullOrEmpty(dienthoai))
+            {
+                ViewData["Loi7"] = "Phải nhập sdt";
+            }
+            else
+            {
+                kh.TenKH = hoten;
+                kh.TaiKhoan = tendn;
+                kh.MatKhau = matkhau;
+                kh.Email = email;
+                kh.DiaChi = diachi;
+                kh.DienThoai = dienthoai;
+                db.KHACHHANGs.InsertOnSubmit(kh);
+                db.SubmitChanges();
+                return RedirectToAction("DangNhap");
+            }
+            return this.DangKy();
+        }
+        [HttpGet]
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DangNhap(FormCollection collection)
+        {
+            var tendn = collection["TenND"];
+            var matkhau = collection["Matkhau"];
+            if (String.IsNullOrEmpty(tendn))
+            {
+                ViewData["Loi1"] = "Phải nhập tên đăng nhập";
+            }
+            else if (String.IsNullOrEmpty(matkhau))
+            {
+                ViewData["Loi2"] = "Phải nhâp mật khẩu";
+            }
+            else
+            {
+                KHACHHANG kh = db.KHACHHANGs.SingleOrDefault(n => n.TaiKhoan == tendn && n.MatKhau == matkhau);
+                if (kh != null)
+                {
+                    ViewBag.Thongbao = "Đăng nhập thành công";
+                    Session["TaiKhoan"] = kh;
+                }
+                else
+                    ViewBag.Thongbao = "Tên đăng nhập hoặc mật khẩu không đúng";
+            }
+            return RedirectToAction("Index", "Sanpham");
+        }
+    }
+}
